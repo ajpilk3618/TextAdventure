5c5b7bc7e708973fa128d864fc1836c55c795bb3
AJ <apilkerton3618@gmail.com>
2019-12-03 18:48:30 -0600

    second commit still incomplete

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace TheThing
 {
@@ -12,6 +13,7 @@ namespace TheThing
         public static void titleScreen()
         {
             Console.WriteLine("Welcome to the game");
+            Thread.Sleep(3000);
             Console.WriteLine("Press 'Enter' to begin");
             Console.ReadLine();
             Console.Clear();
@@ -21,14 +23,20 @@ namespace TheThing
         public static void first()
         {
             string option;
-            Console.WriteLine("You wake up in the middle of a jungle in the drivers seat of a crashed SUV.");
+            Console.WriteLine("You wake up in the middle of the jungle in the drivers seat of a crashed SUV.");
+            Thread.Sleep(3000);
             Console.WriteLine("Overhead, you see a helicopter and a dog barks nearby.");
+            Thread.Sleep(3000);
             Console.WriteLine("You also see a military compound in the distance.");
+            Thread.Sleep(3000);
+            Console.WriteLine();
             Console.WriteLine("What do you investigate?");
             Console.WriteLine("1: The helicopter");
             Console.WriteLine("2: The dog");
             Console.WriteLine("3: The compound");
             Console.WriteLine("4: You run away");
+            Thread.Sleep(2000);
+            Console.WriteLine();
             Console.Write("Option # ");
             option = Console.ReadLine();
             Console.Clear();
@@ -39,46 +47,54 @@ namespace TheThing
                 // Helicopter
                     {
                         Console.WriteLine("You run after the helicopter and it starts shooting. You take cover but does not seem to be shooting at you.");
+                        Thread.Sleep(3000);
                         Console.WriteLine("You follow the helicopter, and it comes upon a military compound.");
-                        Console.WriteLine("The helicopter is quickly shot down by the military and you nervoulsy approach the area.");
+                        Thread.Sleep(3000);
+                        Console.WriteLine("The helicopter is quickly shot down by the military and you nervoulsy approach the area where a member of the crew has already taken in the dog.");
+                        Thread.Sleep(3000);
                         Console.WriteLine("The crew surrounds you and asks for your information.");
                         Console.WriteLine("Press 'Enter' to continue.");
                         Console.ReadLine();
                         Console.Clear();
                         
-                        second();
+                        getInfo();
                         break;
                     }
                 case "2":
                 // Dog
                     {
                         Console.WriteLine("You find the dog and help it escape the helicopters attacks.");
+                        Thread.Sleep(3000);
                         Console.WriteLine("The helicopter is eventually shot down by the military and you nervoulsy approach the area with the dog.");
+                        Thread.Sleep(3000);
                         Console.WriteLine("The crew surrounds you and asks for your information.");
                         Console.WriteLine("Press 'Enter' to continue.");
                         Console.ReadLine();
                         Console.Clear();
 
-                        second1();
+                        veryDead();
                         break;
                     }
                 case "3":
                 // Compound
                     {
-                        Console.WriteLine("You seek the protection of the military and approach the area with caution.");
+                        Console.WriteLine("You seek the protection of the military and approach the area with caution where a member of the crew has already taken in the dog.");
+                        Thread.Sleep(3000);
                         Console.WriteLine("The crew surrounds you and asks for your information.");
                         Console.WriteLine("Press 'Enter' to continue.");
                         Console.ReadLine();
                         Console.Clear();
 
-                        second();
+                        getInfo();
                         break;
                     }
                 case "4":
                 // Run away
                     {
                         Console.WriteLine("You run away from everything.");
-                        Console.WriteLine("In your haste you trip on a root and brake your leg.");
+                        Thread.Sleep(3000);
+                        Console.WriteLine("In your haste to escape the danger you trip on a root and brake your leg.");
+                        Thread.Sleep(3000);
                         Console.WriteLine("Unable to move, you eventually die.");
                         Console.WriteLine("Press 'Enter' to continue");
                         Console.ReadLine();
@@ -88,37 +104,157 @@ namespace TheThing
                         break;
                     }
             }
-
         }
 
-        public static void second()
+        public static void getInfo()
         {
+            Console.WriteLine("'Give us your name and we might let you live' The leader shouts.");
+            Thread.Sleep(3000);
+            Console.WriteLine();
+
             Console.Write("Please enter your first name: ");
-            string Firstname = Console.ReadLine();
+            string firstName = Console.ReadLine();
             Console.Write("Please enter your last name: ");
-            string Lastname = Console.ReadLine();
+            string lastName = Console.ReadLine();
+            Console.Write("Please enter your profession: ");
+            string job = Console.ReadLine(); 
 
-            Console.WriteLine("The crew trusts you and allows you into the compound.");
+            Random rnd = new Random();
+            int[] prob = { 1, 2, 3, 4, 5 };
+            int randomNumber = rnd.Next(0, 5);
+            int num = prob[randomNumber];
+            if (num == 2)
+            {
+                Console.WriteLine("The crew does not trust you, and assumes you are a spy.");
+                Thread.Sleep(3000);
+                Console.WriteLine("You are then killed by the crew.");
+                Console.WriteLine("Press 'Enter' to continue");
+                Console.ReadLine();
+                Console.Clear();
+                gameOver();
+            }
+            else
+            {
+                Console.WriteLine($"'Oh! I didn't know you were a {job}.' says the leader.");
+                Console.WriteLine("'Hopefully your skills will be of use to us here.'");
+                Console.WriteLine($"'Will you help us {firstName} {lastName}?' he asks.");
 
-            third();
+                string choice; ;
+                Console.WriteLine("yes or no");
+                choice = Console.ReadLine();
+
+                while (choice == "no")
+                {
+                    Console.WriteLine("'Please take your time and think it over.' the leader says firmly.");
+                    Console.WriteLine("yes or no");
+                    choice = Console.ReadLine();
+                }
+
+                Console.WriteLine("The crew trusts you and allows you into the compound.");
+                Console.Clear();
+                storyTime();
+            }
         }
 
-        public static void second1()
+        public static void veryDead()
+        // CHOICE WITH THE DOG YOU DIE NO MATTER WHAT
         {
+            Console.WriteLine("'Give us your name and we might let you live' The leader shouts.");
+            Thread.Sleep(3000);
+            Console.WriteLine();
+
             Console.Write("Please enter your first name: ");
-            string Firstname = Console.ReadLine();
+            string firstName = Console.ReadLine();
             Console.Write("Please enter your last name: ");
-            string Lastname = Console.ReadLine();
+            string lastName = Console.ReadLine();
+            Console.Write("Please enter your profession: ");
+            string job = Console.ReadLine();
 
-            Console.WriteLine("The crew trusts you and allows you and the dog into the compound.");
-            Console.WriteLine("You join the crew and dog is sent to the kennel with the other animals.");
+            Random rnd = new Random();
+            int[] prob = { 1, 2, 3, 4, 5 };
+            int randomNumber = rnd.Next(0, 5);
+            int num = prob[randomNumber];
+            if (num == 2)
+            {
+                Console.WriteLine("The crew does not trust you, and assumes you are a spy.");
+                Thread.Sleep(3000);
+                Console.WriteLine("You and the dog are then killed by the crew.");
+                Console.WriteLine("Press 'Enter' to continue");
+                Console.ReadLine();
+                Console.Clear();
+                gameOver();
+            }
+            else
+            {
+                Console.WriteLine($"'Oh! I didn't know you were a {job}.' says the leader.");
+                Console.WriteLine("'Hopefully your skills will be of use to us here.'");
+                Console.WriteLine($"'Will you help us {firstName} {lastName}?' he asks.");
 
-            third();
+                string choice;
+                Console.WriteLine("yes or no");
+                choice = Console.ReadLine();
+
+                while (choice == "no")
+                {
+                    Console.WriteLine("'Please take your time and think it over.' the leader says firmly.");
+                    Console.WriteLine("yes or no");
+                    choice = Console.ReadLine();
+                }
+
+                Console.WriteLine("The crew trusts you and allows you and the dog into the compound.");
+                Console.Clear();
+                deathTime();
+            }
         }
 
-        public static void third()
+        public static void deathTime()
         {
+            Console.WriteLine("A few days go by and you are now a member of the crew.");
+            Thread.Sleep(3000);
+            Console.WriteLine("A small group set out to discover where the mysterious helicopter came from.");
+            Thread.Sleep(3000);
+            Console.WriteLine("While they are away the dog you saved suddendly bursts apart killing the other dogs in the kennel.");
+            Thread.Sleep(3000);
+            Console.WriteLine("A grotesque creature emerges that seems to be using the bodies of other dead animals for its form.");
+            Thread.Sleep(3000);
+            Console.WriteLine("You and the captain are successful in securing the area so no one gets injured.");
+            Thread.Sleep(3000);
+            Console.WriteLine("One of the crew uses a flamethrower to kill the creature after it begins to break down the doors.");
+            Thread.Sleep(3000);
+            Console.WriteLine("The crew doctor examines the body later and discovers human bones and tissue are integrated into the creature.");
+            Thread.Sleep(3000);
+            Console.WriteLine("'It's definitely not from earth' the doc says nervously.");
+            Thread.Sleep(3000);
+            Console.WriteLine("'All of this started with that one' the captain points at you and the rest of the crew looks on nervously.");
+            Thread.Sleep(3000);
+            Console.WriteLine();
+            Thread.Sleep(3000);
+            Console.WriteLine("At that moment your body rips apart and begins consuming the burnt carcass of the creature as well as other members of the crew.");
+            Thread.Sleep(2000);
+            Console.WriteLine("Press'Enter' to continue");
+            Console.ReadLine();
 
+            Console.Clear();
+            gameOver();
+        }
+
+        public static void storyTime()
+            // THIS METHOD IS JUST FOR EXPOSITION ON THE STORY NO CHOICES TO BE MADE BY THE PLAYER
+        {
+            Console.WriteLine("A few days go by and you are now a member of the crew.");
+            Thread.Sleep(3000);
+            Console.WriteLine("A small group set out to discover where the mysterious helicopter came from.");
+            Thread.Sleep(3000);
+            Console.WriteLine("The leader assures you that it was a misunderstanding, and that we are not under attack from another nation.");
+            Thread.Sleep(3000);
+            Console.WriteLine("The rest of the crew is back and has made a disturbing discovery.");
+            Thread.Sleep(3000);
+            Console.WriteLine("They tell of the monster they found at a German military base a few miles away.");
+            Thread.Sleep(3000);
+            Console.WriteLine("They also confirmed that the helicopter was sent from the German base.");
+            Thread.Sleep(3000);
+            Console.WriteLine("But no one knows why it seemed to be chasing the dog.");
+            Thread.Sleep(3000);
 
             fourth();
         }
@@ -133,10 +269,10 @@ namespace TheThing
         public static void last()
         {
             Random rnd = new Random();
-            int[] prob = { 1, 2, 3, 4 };
-            int randomNumber = rnd.Next(0, 4);
+            int[] prob = { 1, 2, 3, 4, 5, 6 };
+            int randomNumber = rnd.Next(0, 6);
             int num = prob[randomNumber];
-            if (num == 3)
+            if (num == 2)
             {
                 Console.WriteLine("You were infected by the dog and died");
                 gameOver();
